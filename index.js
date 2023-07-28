8bcf3401d4f7317e2459701f0cf6dbe0878ca3df
Vamsi <gvamsikrishna.mohan@gmail.com>
2023-07-28 18:56:32 +0530

    test

--- index.js
+++ index.js
@@ -1,49 +1,57 @@
 let webex;
 let receiveTranscriptionOption = true;
-let transcript_final_result = ""
+let transcript_final_result = "";
 let meetings;
 let current_meeting;
 webex = window.webex = Webex.init({
   config: {
     logger: {
-      level: 'debug'
+      level: "debug",
     },
     meetings: {
       reconnection: {
-        enabled: true
+        enabled: true,
       },
       enableRtx: true,
       experimental: {
-        enableUnifiedMeetings: true
-      }
-    }
+        enableUnifiedMeetings: true,
+      },
+    },
     // Any other sdk config we need
   },
   credentials: {
-    access_token: "MjA1ZmUzMzgtYjgzMS00ZmFhLWI3ODAtZDdlOTRlNTg1ZWUwZmRjZTEzZWUtZGRh_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
-  }
+    access_token:
+      "MjA1ZmUzMzgtYjgzMS00ZmFhLWI3ODAtZDdlOTRlNTg1ZWUwZmRjZTEzZWUtZGRh_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f",
+  },
 });
 
-webex.once('ready', () => {
-  console.log('Authentication#initWebex() :: Webex Ready');
+webex.once("ready", () => {
+  console.log("Authentication#initWebex() :: Webex Ready");
 });
 
-webex.meetings.register()
-webex.meetings.syncMeetings()
-meetings = webex.meetings.getAllMeetings();
-current_meeting = obj[Object.keys(meetings)[0]];
+webex.meetings
+  .register()
+  .then(() => {
+    webex.meetings.syncMeetings();
+  })
+  .finally(() => {
+    if (webex.meetings.regisered) {
+      meetings = webex.meetings.getAllMeetings();
+      current_meeting = obj[Object.keys(meetings)[0]];
+      current_meeting.on("meeting:receiveTranscription:started", (payload) => {
+        transcript_final_result =
+          transcript_final_result + payload["transcription"];
+        console.log(payload);
+      });
 
-current_meeting.on('meeting:receiveTranscription:started', (payload) => {
-  transcript_final_result = transcript_final_result + payload["transcription"]
-  console.log(payload)
-});
+      const joinOptions = {
+        moveToResource: false,
+        resourceId: webex.devicemanager._pairedDevice
+          ? webex.devicemanager._pairedDevice.identity.id
+          : undefined,
+        receiveTranscription: receiveTranscriptionOption,
+      };
 
-const joinOptions = {
-  moveToResource: false,
-  resourceId: webex.devicemanager._pairedDevice ?
-    webex.devicemanager._pairedDevice.identity.id :
-    undefined,
-  receiveTranscription: receiveTranscriptionOption
-};
-
-current_meeting.join(joinOptions)
+      current_meeting.join(joinOptions);
+    }
+  });

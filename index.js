180da51f94700d8e607446c6b09cc0f67230a337
Vamsi <gvamsikrishna.mohan@gmail.com>
2023-07-28 19:46:43 +0530

    test

--- index.js
+++ index.js
@@ -29,27 +29,34 @@ webex.once("ready", () => {
   console.log("Authentication#initWebex() :: Webex Ready");
 });
 
-webex.meetings
-  .register()
-  .then(() => {
-    console.log("successful registered")
-    webex.meetings.syncMeetings().then(() => new Promise((resolve) => {
-      setTimeout(() => resolve(), 3000);
-    }))
+webex.meetings.register().then(() => {
+  console.log("successful registered");
+  webex.meetings
+    .syncMeetings()
+    .then(
+      () =>
+        new Promise((resolve) => {
+          setTimeout(() => resolve(), 3000);
+        })
+    )
     .then(() => {
-      console.log('MeetingsManagement#collectMeetings() :: successfully collected meetings');
+      console.log(
+        "MeetingsManagement#collectMeetings() :: successfully collected meetings"
+      );
       meetings = webex.meetings.getAllMeetings();
 
       if (webex.meetings.registered) {
-        
         console.log(meetings);
         current_meeting = meetings[Object.keys(meetings)[0]];
         console.log(current_meeting);
-        current_meeting.on("meeting:receiveTranscription:started", (payload) => {
-          transcript_final_result =
-            transcript_final_result + payload["transcription"];
-          console.log(payload);
-        })
+        current_meeting.on(
+          "meeting:receiveTranscription:started",
+          (payload) => {
+            transcript_final_result =
+              transcript_final_result + payload["transcription"];
+            // console.log(payload);
+          }
+        );
       }
       const joinOptions = {
         moveToResource: false,
@@ -58,8 +65,33 @@ webex.meetings
           : undefined,
         receiveTranscription: receiveTranscriptionOption,
       };
-      
-      current_meeting.join(joinOptions);
-    })});
-    
 
+      current_meeting.join(joinOptions);
+    });
+});
+
+const intervalID = setInterval(summary, 10000,);
+
+function summary() {
+  // WARNING: For POST requests, body is set to null by browsers.
+  var data = JSON.stringify({
+    model: "Bart",
+    function: "summarize_text",
+    content:transcript_final_result,
+  });
+  console.log(transcript_final_result)
+
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+      console.log(this.responseText);
+    }
+  });
+
+  xhr.open("POST", "http://127.0.0.1:3000//summary");
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.send(data);
+}

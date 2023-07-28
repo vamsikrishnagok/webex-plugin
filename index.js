e89d80941c349f3ecb69449644b50346cc73177c
Vamsi <gvamsikrishna.mohan@gmail.com>
2023-07-28 19:13:59 +0530

    test

--- index.js
+++ index.js
@@ -33,21 +33,24 @@ webex.meetings
   .register()
   .then(() => {
     console.log("successful registered")
-    webex.meetings.syncMeetings();
-  })
-  .finally(() => {
-    console.log(webex.meetings.regisered)
-    if (webex.meetings.regisered) {
-      meetings = webex.meetings.getAllMeetings();
-      console.log(meetings);
-      current_meeting = meetings[Object.keys(meetings)[0]];
-      console.log(current_meeting);
-      current_meeting.on("meeting:receiveTranscription:started", (payload) => {
-        transcript_final_result =
-          transcript_final_result + payload["transcription"];
-        console.log(payload);
-      });
+    webex.meetings.syncMeetings().then(() => new Promise((resolve) => {
+      setTimeout(() => resolve(), 3000);
+    }))
+    .then(() => {
+      console.log('MeetingsManagement#collectMeetings() :: successfully collected meetings');
+      const meetings = webex.meetings.getAllMeetings();
 
+      if (webex.meetings.regisered) {
+        meetings = webex.meetings.getAllMeetings();
+        console.log(meetings);
+        current_meeting = meetings[Object.keys(meetings)[0]];
+        console.log(current_meeting);
+        current_meeting.on("meeting:receiveTranscription:started", (payload) => {
+          transcript_final_result =
+            transcript_final_result + payload["transcription"];
+          console.log(payload);
+        })
+      }
       const joinOptions = {
         moveToResource: false,
         resourceId: webex.devicemanager._pairedDevice
@@ -57,5 +60,6 @@ webex.meetings
       };
 
       current_meeting.join(joinOptions);
-    }
-  });
+    })});
+    
+

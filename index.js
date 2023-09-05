2a692a25b10e3df68ec38538063370f6f6fa1b11
Vamsi <gvamsikrishna.mohan@gmail.com>
2023-09-05 18:42:13 +0530

    asd

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let receiveTranscriptionOption = true;
 let transcript_final_result = {"transcript":""};
 let meetings;
 let current_meeting;
-
+let WEBEX_ACCESS_TOKEN;
 
 function summary() {
   // WARNING: For POST requests, body is set to null by browsers.
@@ -44,78 +44,87 @@ function summary() {
   xhr.send(data);
 }
 
-
-
-webex = window.webex = Webex.init({
-  config: {
-    logger: {
-      level: "debug",
-    },
-    meetings: {
-      reconnection: {
-        enabled: true,
+// Endpoint should be changed based on where server hosted
+fetch("http://localhost:3000/config")
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json)
+    WEBEX_ACCESS_TOKEN = json.WEBEX_ACCESS_TOKEN
+    webex = window.webex = Webex.init({
+      config: {
+        logger: {
+          level: "debug",
+        },
+        meetings: {
+          reconnection: {
+            enabled: true,
+          },
+          enableRtx: true,
+          experimental: {
+            enableUnifiedMeetings: true,
+          },
+        },
+        // Any other sdk config we need
       },
-      enableRtx: true,
-      experimental: {
-        enableUnifiedMeetings: true,
+      credentials: {
+        access_token:
+        WEBEX_ACCESS_TOKEN,
       },
-    },
-    // Any other sdk config we need
-  },
-  credentials: {
-    access_token:
-      "YjE0MmY1YTEtNGUxMi00MDlhLTk2NjgtODQxODNmZmIwYTFiZTRiNzFmZDgtNzc4_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f",
-  },
-});
-
-webex.once("ready", () => {
-  console.log("Authentication#initWebex() :: Webex Ready");
-});
-
-webex.meetings.register().then(() => {
-  console.log("successful registered");
-  webex.meetings
-    .syncMeetings()
-    .then(
-      () =>
-        new Promise((resolve) => {
-          setTimeout(() => resolve(), 3000);
-        })
-    )
-    .then(() => {
-      console.log(
-        "MeetingsManagement#collectMeetings() :: successfully collected meetings"
-      );
-      meetings = webex.meetings.getAllMeetings();
-
-      if (webex.meetings.registered) {
-        console.log(meetings);
-        current_meeting = meetings[Object.keys(meetings)[0]];
-        console.log(current_meeting);
-        current_meeting.on(
-          "meeting:receiveTranscription:started",
-          (payload) => {
-            if (payload["type"]=="transcript_final_result"){
-              transcript_final_result["transcript"] = transcript_final_result["transcript"] + ", " + payload["transcription"];
-              
-            }
-           
-            console.log(transcript_final_result)
-            
-          }
-        );
-      }
-      const joinOptions = {
-        moveToResource: false,
-        resourceId: webex.devicemanager._pairedDevice
-          ? webex.devicemanager._pairedDevice.identity.id
-          : undefined,
-        receiveTranscription: receiveTranscriptionOption,
-      };
-
-      current_meeting.join(joinOptions);
     });
-});
+    webex.once("ready", () => {
+      console.log("Authentication#initWebex() :: Webex Ready");
+    });
+    
+    webex.meetings.register().then(() => {
+      console.log("successful registered");
+      webex.meetings
+        .syncMeetings()
+        .then(
+          () =>
+            new Promise((resolve) => {
+              setTimeout(() => resolve(), 3000);
+            })
+        )
+        .then(() => {
+          console.log(
+            "MeetingsManagement#collectMeetings() :: successfully collected meetings"
+          );
+          meetings = webex.meetings.getAllMeetings();
+    
+          if (webex.meetings.registered) {
+            console.log(meetings);
+            current_meeting = meetings[Object.keys(meetings)[0]];
+            console.log(current_meeting);
+            current_meeting.on(
+              "meeting:receiveTranscription:started",
+              (payload) => {
+                if (payload["type"]=="transcript_final_result"){
+                  transcript_final_result["transcript"] = transcript_final_result["transcript"] + ", " + payload["transcription"];
+                  
+                }
+               
+                console.log(transcript_final_result)
+                
+              }
+            );
+          }
+          const joinOptions = {
+            moveToResource: false,
+            resourceId: webex.devicemanager._pairedDevice
+              ? webex.devicemanager._pairedDevice.identity.id
+              : undefined,
+            receiveTranscription: receiveTranscriptionOption,
+          };
+    
+          current_meeting.join(joinOptions);
+        });
+    });
+  });
+
+console.log(WEBEX_ACCESS_TOKEN)
+
+
+
 
 const intervalID = setInterval(summary, 100000);
 
